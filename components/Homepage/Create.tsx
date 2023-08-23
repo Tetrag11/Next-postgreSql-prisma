4ab6a0b815e5df9d95c77a74a1fd4242ec6341bb
Tetrag11 <tehanekanayake@gmail.com>
2023-08-23 19:33:49 +0530

    make finn and jake move

--- components/Homepage/Create.tsx
+++ components/Homepage/Create.tsx
@@ -6,13 +6,16 @@ import AnimationContext from "../WaltDisney/AnimationContext";
 export default function Create({ setX, calculateBackgroundColor }: any) {
   const context = useContext(AnimationContext) as any;
   const foreground = context.foregroundAnimation;
-  const [mousePos, setMousePos] = useState({}) as any;
-  const container = useRef(null) as any;
+  const moveFinn = context.moveFinn;
+  const moveJake = context.moveJake;
+  const container = context.container;
+  const finn = context.finn;
+  const jake = context.jake;
 
   useEffect(() => {
     const handleMouseMove = (event: any) => {
-      setMousePos({ x: event.clientX });
       context.setX(event.clientX);
+      context.setY(event.clientY);
     };
 
     container.current &&
@@ -29,13 +32,15 @@ export default function Create({ setX, calculateBackgroundColor }: any) {
   return (
     <animated.div className="w-full h-full pt-28 " ref={container}>
       {/* made a relative wrapper, and inside it, has a relative child, which results in the parent taking the relative childs height, and then pushes the relative child down by margin-top, doing this will fix all of your height problems when dealing with absolute positions, and the parent only takes the required space  */}
-      <div className="wrapper sm:max-w-[735px] max-w-[300px]  w-full mx-auto relative overflow-hidden">
+      <div className="wrapper sm:max-w-[735px] max-w-[300px]  w-full mx-auto relative flex  items-center">
         <animated.div
-          style={foreground}
+          ref={finn}
+          style={moveFinn}
           className="absolute sm:w-[300px] w-[200px] sm:h-[400px] h-[300px] border-2 border-black top-0 "
         ></animated.div>
         <animated.div
-          style={foreground}
+          ref={jake}
+          style={moveJake}
           className="relative sm:w-[300px] w-[200px]  sm:h-[400px] h-[300px] border-2 border-black mt-[20%] sm:left-[16rem] left-[10rem] translate-x-[-50%] scale-[80%] "
         ></animated.div>
         <div className="absolute w-full h-full sm:top-[55%] top-[100%] translate-y-[-50%] sm:left-[45%] left-[30%]">
--- components/WaltDisney/AnimationController.tsx
+++ components/WaltDisney/AnimationController.tsx
@@ -1,12 +1,18 @@
 "use client";
-import React from "react";
+import React, { useRef } from "react";
 import { createContext, useContext, useState, useEffect } from "react";
 import AnimationContext from "./AnimationContext";
 import { useSpring } from "@react-spring/web";
 
+const constrain = 500;
+
 export default function AnimationController({ children }: any) {
   const [x, setX] = useState(false);
+  const [y, setY] = useState(false);
   const [windowWidth, setWindowWidth] = useState(0);
+  const container = useRef(null);
+  const finn = useRef(null);
+  const jake = useRef(null);
 
   const calculateBackgroundColor = (xPosition: any, background: boolean) => {
     if (background) {
@@ -27,20 +33,90 @@ export default function AnimationController({ children }: any) {
     setWindowWidth(window.innerWidth / 2);
   }, []);
 
+  // stuff that changes background color
   const backgroundAnimation = useSpring({
-    backgroundColor: calculateBackgroundColor(x, true), // Initial position
-    config: { tension: 200, friction: 20 },
+    // backgroundColor: calculateBackgroundColor(x, true), // Initial position
+    // config: { tension: 200, friction: 20 },
   });
 
   const foregroundAnimation = useSpring({
-    color: calculateBackgroundColor(x, false),
-    borderColor: calculateBackgroundColor(x, false),
-    config: { tension: 200, friction: 20 },
+    // color: calculateBackgroundColor(x, false),
+    // borderColor: calculateBackgroundColor(x, false),
+    // config: { tension: 200, friction: 20 },
   });
+
+  // stuff that makes finn and jake move
+
+  const calculatePerspective = (
+    x: any,
+    y: any,
+    container: any,
+    finn: any,
+    jake: any,
+    human: boolean
+  ) => {
+    if (finn.current && jake.current) {
+      if (human) {
+        let finnInfo = finn.current.getBoundingClientRect();
+
+        let calcX = -(y - finnInfo.y - finnInfo.height / 2) / constrain;
+        let calcY = (x - finnInfo.x - finnInfo.width / 2) / constrain;
+
+        return (
+          "perspective(100px) " +
+          "   rotateX(" +
+          calcX +
+          "deg) " +
+          "   rotateY(" +
+          calcY +
+          "deg) "
+        );
+      } else {
+        let jakeInfo = jake.current.getBoundingClientRect();
+
+        let calcX = (y - jakeInfo.y - jakeInfo.height / 2) / constrain;
+        let calcY = -(x - jakeInfo.x - jakeInfo.width / 2) / constrain;
+
+        return (
+          "perspective(100px) " +
+          "   rotateX(" +
+          calcX +
+          "deg) " +
+          "   rotateY(" +
+          calcY +
+          "deg) "
+        );
+      }
+    }
+  };
+
+  const moveFinn = {
+    transform: calculatePerspective(x, y, container, finn, jake, true),
+
+    config: { tension: 200, friction: 20 },
+  };
+
+  const moveJake = {
+    transform: calculatePerspective(x, y, container, finn, jake, false),
+
+    config: { tension: 200, friction: 20 },
+  };
 
   return (
     <AnimationContext.Provider
-      value={{ x, setX, backgroundAnimation, foregroundAnimation }}
+      value={{
+        x,
+        setX,
+        y,
+        setY,
+        backgroundAnimation,
+        foregroundAnimation,
+        finn,
+        jake,
+        container,
+        moveFinn,
+        moveJake,
+      }}
     >
       {children}
     </AnimationContext.Provider>

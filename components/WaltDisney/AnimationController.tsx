6c2a9435f2e735de9cacc3941d25ddb9aec9b0f1
Tetrag11 <tehanekanayake@gmail.com>
2023-08-23 17:58:34 +0530

    fix for window not defined

--- components/WaltDisney/AnimationController.tsx
+++ components/WaltDisney/AnimationController.tsx
@@ -1,28 +1,31 @@
 "use client";
 import React from "react";
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 import AnimationContext from "./AnimationContext";
 import { useSpring } from "@react-spring/web";
 
 export default function AnimationController({ children }: any) {
   const [x, setX] = useState(false);
-  const [background, setBackground] = useState({});
+  const [windowWidth, setWindowWidth] = useState(0);
 
   const calculateBackgroundColor = (xPosition: any, background: boolean) => {
     if (background) {
-      if (xPosition <= window.innerWidth / 2) {
+      if (xPosition <= windowWidth) {
         return "white";
       } else {
         return "black";
       }
     } else {
-      if (xPosition >= window.innerWidth / 2) {
+      if (xPosition >= windowWidth) {
         return "white";
       } else {
         return "black";
       }
     }
   };
+  useEffect(() => {
+    setWindowWidth(window.innerWidth / 2);
+  }, []);
 
   const backgroundAnimation = useSpring({
     backgroundColor: calculateBackgroundColor(x, true), // Initial position
